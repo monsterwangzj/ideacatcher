ff6e2883c25ff1bab1e610f9a6b2690842fa0e2e
乘义 <zhaojun.wzj@alibaba-inc.com>
2015-08-16 12:01:17 +0800

    init project files

---
+++ ideacatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ideacatcher
+//
+//  Created by zhaojun.wzj on 8/16/15.
+//  Copyright (c) 2015 softwisdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ideacatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ideacatcher
+//
+//  Created by zhaojun.wzj on 8/16/15.
+//  Copyright (c) 2015 softwisdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
